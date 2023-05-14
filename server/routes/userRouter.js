5254357d076fb45dfff795998492ecdeac47554a
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2023-05-14 20:37:12 -0300

    feat: Cria endpoint com método get para lidar com o token enviado pelo frontend

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 router.post('/login', express.json(), userController.login);
 router.post('/register', express.json(), userController.register);
+
+router.get('/', express.json(), authController, (req, res) => {
+  const user = req.user;
+  res.status(200).json(user);
+});
 
 module.exports = router;
