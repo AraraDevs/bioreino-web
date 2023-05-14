f3f4be0e7daf49890e55f1b1c6de430325497d5b
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2023-05-14 20:30:22 -0300

    chore: Adiciona o UserStorage no App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,19 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './Components/Login/Login';
 import Home from './Components/Home/Home';
+import { UserStorage } from './UserContext';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <UserStorage>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </UserStorage>
     </>
   );
 }
