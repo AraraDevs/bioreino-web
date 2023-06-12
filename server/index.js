ecf4373c45c7726344e1629566990d814713cbc7
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2023-06-12 19:17:17 -0300

    fix: Corrige o path.join no node que não conseguia enviar os arquivos para o navegador através dos métodos de requisição quando não está na raiz do projeto

--- server/index.js
+++ server/index.js
@@ -39,10 +39,6 @@ if (process.env.NODE_ENV !== 'development') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
   app.get('*', (req, res) => {
-    res.sendFile(
-      path.join(__dirname, '../client/dist/index.html', (error) => {
-        if (error) res.status(500).send(error);
-      }),
-    );
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
