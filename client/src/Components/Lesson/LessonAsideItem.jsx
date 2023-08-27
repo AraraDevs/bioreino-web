db154d4dd1b0accf351aba4ca322fa94bf1e4b51
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2023-08-27 20:05:50 -0300

    fix: Altera o caminho para acessar o arquivo FormatURL

--- client/src/Components/Lesson/LessonAsideItem.jsx
+++ client/src/Components/Lesson/LessonAsideItem.jsx
@@ -4,13 +4,13 @@ import { NavLink, useParams } from 'react-router-dom';
 import { ReactComponent as OpenClapperboard } from '../../Assets/claquete_aberta.svg';
 import { ReactComponent as ClosedClapperboard } from '../../Assets/claquete_fechada.svg';
 import { UserContext } from '../../Context/UserContext';
-import formatUrl from '../Helper/formatUrl';
+import FormatURL from '../Helper/FormatURL';
 
 const LessonAsideItem = ({ lesson, courseName, courseUrlName }) => {
   const { data } = React.useContext(UserContext);
   const [userCoursesProgress, setCoursesProgress] = React.useState(null);
   const [lessonViewed, setLessonViewed] = React.useState(false);
-  const lessonUrl = formatUrl(lesson.title);
+  const lessonURL = FormatURL(lesson.title);
   const { lesson: lessonUrlName } = useParams();
 
   React.useEffect(() => {
@@ -27,7 +27,7 @@ const LessonAsideItem = ({ lesson, courseName, courseUrlName }) => {
   return (
     <li className={styles.listItem}>
       <NavLink
-        to={`/curso/${courseUrlName}/${lessonUrl}`}
+        to={`/curso/${courseUrlName}/${lessonURL}`}
         className={styles.link}
       >
         {lessonViewed ? (
