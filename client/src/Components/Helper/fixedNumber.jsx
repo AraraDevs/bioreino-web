07b25506b6be4c59435a65767ebfc573d4568d8c
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2023-11-01 01:22:50 -0300

    feat: Adiciona substituição de . por , após adicionar casas decimais no número

--- client/src/Components/Helper/fixedNumber.jsx
+++ client/src/Components/Helper/fixedNumber.jsx
@@ -1,5 +1,5 @@
 const fixedNumber = (value, numberOfDecimals = 2) => {
-  return value.toFixed(numberOfDecimals);
+  return value.toFixed(numberOfDecimals).replace('.', ',');
 };
 
 export default fixedNumber;
