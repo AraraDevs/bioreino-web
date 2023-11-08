73eacbc8b397f9db29fd3ca5c96f27ac9a67e5c6
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2023-11-08 18:16:48 -0300

    build: Remove dependencia do vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr';
-import { VitePluginRadar } from 'vite-plugin-radar';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,11 +12,6 @@ export default defineConfig({
       include: ['./src/**/*.js', './src/**/*.jsx'],
     }),
     svgr(),
-    VitePluginRadar({
-      analytics: {
-        id: 'G-9CX73DLPH8',
-      },
-    }),
   ],
   server: {
     proxy: {
