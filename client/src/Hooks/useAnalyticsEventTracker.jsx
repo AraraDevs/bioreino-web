6bcede96ead7f8b5ed0d96b852af621850959e72
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2023-11-09 23:10:55 -0300

    chore: Remove reactga.debug

--- client/src/Hooks/useAnalyticsEventTracker.jsx
+++ client/src/Hooks/useAnalyticsEventTracker.jsx
@@ -2,8 +2,6 @@ import ReactGA from 'react-ga4';
 
 const useAnalyticsEventTracker = (category) => {
   const eventTracker = (action, label) => {
-    ReactGA.debug();
-
     ReactGA.event({ category, action, label });
   };
   return eventTracker;
