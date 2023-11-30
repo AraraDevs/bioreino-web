39e70a1d63958bdc880bab254c537dcbd7ed28c0
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2023-11-30 01:36:44 -0300

    perf: Aperfeiçoa a performance adicionando lazy e suspense

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import ReactGA from 'react-ga4';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Auth from './Components/Pages/Auth';
-import Home from './Components/Pages/Home';
 import { UserStorage } from './Context/UserContext';
-import Dashboard from './Components/Pages/Dashboard';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
-import Lesson from './Components/Pages/Lesson';
+
+const Home = React.lazy(() => import('./Components/Pages/Home'));
+const Auth = React.lazy(() => import('./Components/Pages/Auth'));
+const Dashboard = React.lazy(() => import('./Components/Pages/Dashboard'));
+const Lesson = React.lazy(() => import('./Components/Pages/Lesson'));
 
 const TRACKING_ID = 'G-9CX73DLPH8';
 ReactGA.initialize(TRACKING_ID);
@@ -24,26 +25,28 @@ function App() {
     <>
       <BrowserRouter>
         <UserStorage>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Auth />} />
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/curso/:course/:lesson?"
-              element={
-                <ProtectedRoute>
-                  <Lesson />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
+          <React.Suspense fallback={<p>Carregando...</p>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login/*" element={<Auth />} />
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/curso/:course/:lesson?"
+                element={
+                  <ProtectedRoute>
+                    <Lesson />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </React.Suspense>
         </UserStorage>
       </BrowserRouter>
     </>
