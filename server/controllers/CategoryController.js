61c8edb8afea63d00cc85d07b1b270f20b658876
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2023-12-14 12:30:32 -0300

    refactor: Retira necessidade de autenticação para buscar por categorias

--- server/controllers/CategoryController.js
+++ server/controllers/CategoryController.js
@@ -3,26 +3,15 @@ const Student = require('../models/Student');
 
 class CategoryController {
   static async getCategories(req, res) {
-    const userId = req.user.id;
-
     try {
-      // get user data
-      const userSaved = await Student.findById(userId);
-
-      const userPlan = userSaved.plan;
-
-      let categories;
-      if (userPlan === 'professional') {
-        categories = await Category.find();
-      } else {
-        categories = await Category.find({ plan: userPlan });
-      }
+      const categories = await Category.find();
 
       res.status(200).json(categories);
     } catch (err) {
       console.log(err);
       res.status(404).json({
-        message: 'Ops! Algum erro ocorreu e não foi possível buscar nenhuma categoria de cursos',
+        message:
+          'Ops! Algum erro ocorreu e não foi possível buscar nenhuma categoria de cursos',
       });
     }
   }
--- server/routes/categoryRouter.js
+++ server/routes/categoryRouter.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
+
 const CategoryController = require('../controllers/CategoryController');
 
-// middlewares
-const verifyToken = require('../helpers/verify-token');
-
-router.get('/', verifyToken, CategoryController.getCategories);
+router.get('/', CategoryController.getCategories);
 
 module.exports = router;
