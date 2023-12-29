c91c3781069190cab0233d7ce22a22d1be031211
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2023-12-29 20:42:26 -0300

    feat(route): add no index do servidor importação da nova api

--- server/index.js
+++ server/index.js
@@ -11,12 +11,14 @@ const userRouter = require('./routes/userRouter');
 const courseRouter = require('./routes/courseRouter');
 const lessonRouter = require('./routes/lessonRouter');
 const categoryRouter = require('./routes/categoryRouter');
+const planRouter = require('./routes/planRouter');
 
 app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/course', courseRouter);
 app.use('/api/lesson', lessonRouter);
 app.use('/api/category', categoryRouter);
+app.use('/api/plan', planRouter);
 
 if (process.env.NODE_ENV !== 'development') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
