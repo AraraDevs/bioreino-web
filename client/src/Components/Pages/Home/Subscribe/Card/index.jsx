89ce56112d5acf4522f0bd405ff4cd0a2e6f582b
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2024-01-02 20:53:22 -0300

    refactor(props): adiciona novas props

--- client/src/Components/Pages/Home/Subscribe/Card/index.jsx
+++ client/src/Components/Pages/Home/Subscribe/Card/index.jsx
@@ -3,10 +3,10 @@ import stylesButton from 'Components/Forms/Button.module.css';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Checked } from 'src/Assets/checked.svg';
 
-const Card = ({ title, benefits, price }) => {
+const Card = ({ _id, name, benefits, price }) => {
   return (
     <div className={styles.card}>
-      <h2 className={styles.title}>{title}</h2>
+      <h2 className={styles.title}>{name}</h2>
       <ul>
         {benefits.map((benefit) => (
           <li key={benefit} className={styles.listItem}>
@@ -17,7 +17,7 @@ const Card = ({ title, benefits, price }) => {
       </ul>
       <p className={styles.price}>R$ {price}</p>
       <div className={styles.signButton}>
-        <Link to={`/login/inscreva/${title}`} className={stylesButton.button}>
+        <Link to={`/login/inscreva/${_id}`} className={stylesButton.button}>
           Assine já!
         </Link>
       </div>
