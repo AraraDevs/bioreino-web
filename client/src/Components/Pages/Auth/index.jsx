12a9674cbb823325437e593b7c863c48b95a7bd3
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2024-01-29 22:43:00 -0300

    chore(auth): página auth não é mais responsável por trocar de página caso o usuário esteja logado

--- client/src/Components/Pages/Auth/index.jsx
+++ client/src/Components/Pages/Auth/index.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
-import { UserContext } from 'Context/User';
 
 const Auth = () => {
-  const { login } = React.useContext(UserContext);
-
-  if (login) return <Navigate to="/dashboard" />;
   return (
     <>
       <Routes>
