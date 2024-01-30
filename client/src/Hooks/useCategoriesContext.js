172f0cd903417608280aff6763ab3cfcac9f7a10
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2024-01-29 23:00:22 -0300

    chore(context): verifica se o custom hook está sendo chamado dentro do provider de categories

--- client/src/Hooks/useCategoriesContext.js
+++ client/src/Hooks/useCategoriesContext.js
@@ -4,6 +4,11 @@ import { CategoriesContext } from 'Context/Categories';
 export default function useCategoriesContext() {
   const { categories, selectedCategory, setSelectedCategory } =
     React.useContext(CategoriesContext);
+  if (!categories && !selectedCategory && !setSelectedCategory) {
+    throw new Error(
+      'useCategoriesContext deve ser usado dentro de um CategoriesProvider'
+    );
+  }
 
   const resetSelectedCategory = React.useCallback(() => {
     setSelectedCategory({});
