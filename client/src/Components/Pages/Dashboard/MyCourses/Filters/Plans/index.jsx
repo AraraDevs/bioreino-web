4ab2ff72cc21a5044f3c466210b138e3f39a491f
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2024-01-30 00:05:56 -0300

    feat(select): add select personalizado e aciona modal

--- client/src/Components/Pages/Dashboard/MyCourses/Filters/Plans/index.jsx
+++ client/src/Components/Pages/Dashboard/MyCourses/Filters/Plans/index.jsx
@@ -1,35 +1,58 @@
 import React from 'react';
+import stylesSelect from 'Components/Forms/Select.module.css';
 import { PlansContext } from 'Context/Plans';
+import bus from 'Components/Helper/bus';
+import Select from 'Components/Forms/Select';
 
 const Plans = ({ selectedPlan, setSelectedPlan, user }) => {
   const { plans } = React.useContext(PlansContext);
-  const currentPlan = plans.find((plan) => plan._id === selectedPlan);
   const userPlan = plans.find((plan) => plan._id === user.plan);
+  const currentPlan = plans.find((plan) => plan._id === selectedPlan);
 
-  function handleChange({ target }) {
-    if (currentPlan._id === target.value) return;
-    setSelectedPlan(target.value);
+  function showModal() {
+    bus.emit('modal', {
+      title: 'Aprimorar plano de assinatura',
+      description: `Para ter acesso aos cursos deste plano é necessário aprimorar o seu plano atual <strong>${userPlan.name.toUpperCase()}</strong> para o plano <strong>PROFESSIONAL</strong>. Gostaria de ser redirecionado para a página de cadastro?`,
+      textDeny: 'Deixar pra depois',
+      textConfirm: 'Vamos nessa!',
+      anchor: {
+        active: true,
+        href: '/login/inscreva/6577d38d69c149d4293871ec',
+      },
+    });
+  }
+
+  function handleChange(id) {
+    if (currentPlan._id === id) return;
+    setSelectedPlan(id);
+  }
+
+  function customSelectItems(plan, setValue) {
+    const planIsFullAccess = !!plan.fullaccess;
+    const userHasFullAccess = planIsFullAccess && !!userPlan.fullaccess;
+    const itemDisabled = !userHasFullAccess && planIsFullAccess;
+
+    return (
+      <li
+        key={plan._id}
+        className={`${stylesSelect.option} ${
+          currentPlan._id === plan._id ? stylesSelect.active : ''
+        } ${itemDisabled && stylesSelect.disabled}`}
+        onClick={itemDisabled ? showModal : () => setValue(plan._id)}
+      >
+        {plan.name}
+      </li>
+    );
   }
 
   return (
-    <select name="plans" value={selectedPlan} onChange={handleChange}>
-      {plans.map((plan) => {
-        if (plan.fullaccess && !userPlan.fullaccess) {
-          return (
-            <option key={plan._id} value={currentPlan._id} disabled>
-              {plan.name}
-            </option>
-          );
-        }
-
-        return (
-          <option key={plan._id} value={plan._id}>
-            {plan.name}
-          </option>
-        );
-      })}
-    </select>
+    <Select
+      options={plans}
+      value={currentPlan.name}
+      setValue={handleChange}
+      customArrayMap={customSelectItems}
+    />
   );
 };
 
-export default React.memo(Plans);
+export default Plans;
