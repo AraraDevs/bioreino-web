34bc528ff26f100af0b7e194bfeb976e9659acfd
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2024-01-29 23:26:20 -0300

    feat(select): adiciona um select personalizado

--- client/src/Components/Pages/Dashboard/MyCourses/Filters/Categories/index.jsx
+++ client/src/Components/Pages/Dashboard/MyCourses/Filters/Categories/index.jsx
@@ -1,35 +1,28 @@
 import React from 'react';
 import useCategoriesContext from 'Hooks/useCategoriesContext';
+import Select from 'Components/Forms/Select';
 
 const Categories = ({ selectedPlan }) => {
   const { categories, selectedCategory, setSelectedCategory } =
     useCategoriesContext();
-  const [filter, setFilter] = React.useState([]);
+  const [filter, setFilter] = React.useState(categories);
 
   React.useEffect(() => {
     const filteredCategories = categories.filter(
-      (category) => !category.plan || category.plan === selectedPlan,
+      (category) => !category.plan || category.plan === selectedPlan
     );
     setFilter(filteredCategories);
   }, [categories, selectedPlan]);
 
   return (
-    <select
-      name="categories"
-      value={selectedCategory?.value || ''}
-      onChange={({ target }) => {
-        const category = categories.find(
-          (category) => category.value === target.value,
-        );
+    <Select
+      options={filter}
+      value={selectedCategory?.name || filter[0].name}
+      setValue={(id) => {
+        const category = categories.find((category) => category._id === id);
         setSelectedCategory(category || null);
       }}
-    >
-      {filter.map((category) => (
-        <option key={category._id} value={category.value}>
-          {category.name}
-        </option>
-      ))}
-    </select>
+    />
   );
 };
 
