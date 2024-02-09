be0f3bc22d4c1155a692d1c68657a8f8329fbf74
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2024-02-09 16:32:26 -0300

    chore: substitui modo de usar o modal

--- client/src/Components/Pages/Dashboard/MyCourses/Filters/Plans/index.jsx
+++ client/src/Components/Pages/Dashboard/MyCourses/Filters/Plans/index.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import stylesSelect from 'Components/Forms/Select.module.css';
 import { PlansContext } from 'Context/Plans';
 import Select from 'Components/Forms/Select';
-import showModal from 'Components/Helper/showModal';
+import useModal from 'Hooks/useModal';
 
 const Plans = ({ selectedPlan, setSelectedPlan, user }) => {
   const { plans } = React.useContext(PlansContext);
   const userPlan = plans.find((plan) => plan._id === user.plan);
   const currentPlan = plans.find((plan) => plan._id === selectedPlan);
+  const { setModal } = useModal();
 
   const modalProperties = {
     title: 'Um momento, amigo! 🧐',
-    description: `Para ter acesso às aulas do plano <strong>PROFESSIONAL</strong>, você precisa atualizar seu plano atual (<strong>${userPlan.name.toUpperCase()}</strong>)!!`,
-    textDeny: 'Deixar pra depois',
-    textConfirm: 'Atualizar plano',
+    desc: `Para ter acesso às aulas do plano <strong>PROFESSIONAL</strong>, você precisa atualizar seu plano atual (<strong>${userPlan.name.toUpperCase()}</strong>)!!`,
+    txtDeny: 'Deixar pra depois',
+    txtConfirm: 'Atualizar plano',
     href: '/assinar/professional',
   };
 
@@ -34,7 +35,14 @@ const Plans = ({ selectedPlan, setSelectedPlan, user }) => {
           currentPlan._id === plan._id ? stylesSelect.active : ''
         } ${itemDisabled && stylesSelect.disabled}`}
         onClick={() => {
-          if (itemDisabled) showModal(modalProperties);
+          if (itemDisabled)
+            setModal(
+              modalProperties.title,
+              modalProperties.desc,
+              modalProperties.txtDeny,
+              modalProperties.txtConfirm,
+              modalProperties.href
+            );
           else setValue(plan._id);
         }}
       >
