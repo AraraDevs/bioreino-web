f0bc8deb24788660b7699e5e3fcbef09b8bc9539
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2024-04-08 22:06:05 -0300

    fix(user): add opcional chaining em dado do usuário

--- client/src/Components/Pages/Dashboard/Header/index.jsx
+++ client/src/Components/Pages/Dashboard/Header/index.jsx
@@ -6,12 +6,13 @@ import { UserContext } from 'Context/User';
 
 const Header = () => {
   const { data, userLogout } = React.useContext(UserContext);
+
   return (
     <header className={`container ${styles.header}`}>
       <nav className={styles.nav}>
         <LogoBioreino />
-        <div className={`${styles.login}`}>
-          <p className={styles.name}>Olá, {data.name.split(' ')[0]}!</p>
+        <div className={styles.login}>
+          <p className={styles.name}>Olá, {data?.name.split(' ')[0]}!</p>
           <button onClick={userLogout} className={styles.logout}>
             <Sair aria-label="Sair da conta" />
           </button>
