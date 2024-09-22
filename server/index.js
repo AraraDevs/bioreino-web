a741f9516a76e7339889fef9f2ca1e3de0274513
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2024-09-22 20:12:33 -0300

    test(cors): comment cors method

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
-const cors = require('cors');
+// const cors = require('cors');
 const express = require('express');
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 const routes = require('./routes/index');
 routes.forEach((route) => app.use('/api', route));
