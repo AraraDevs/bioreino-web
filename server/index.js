35fd2e67c4ecd5d7307ab5fa05780ac2990873d2
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2024-09-22 20:25:21 -0300

    build(cors): remove cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 
-// const cors = require('cors');
 const express = require('express');
 const app = express();
 
-// app.use(cors());
 app.use(express.json());
 const routes = require('./routes/index');
 routes.forEach((route) => app.use('/api', route));
