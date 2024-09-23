ee4a67629fed869930223f8d44063e5a52789f5b
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2024-09-23 00:56:42 -0300

    refactor(api-key): standardizes how you access an object's x-api-key property

--- server/helpers/verify-api-key.js
+++ server/helpers/verify-api-key.js
@@ -1,6 +1,6 @@
 // Middleware para verificar a chave de API
 const verifyApiKey = (req, res, next) => {
-  const keyApi = req.header('x-api-key');
+  const keyApi = req.headers['x-api-key'];
 
   if (!keyApi || keyApi !== process.env.API_KEY) {
     return res
